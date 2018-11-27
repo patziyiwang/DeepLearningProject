b9162ca69baf6fb5ce7a7e4da6fd197283da8518
Pat Wang <patwang@lawn-128-61-124-201.lawn.gatech.edu>
2018-11-27 15:44:05 -0500

    added the draw bounding box function

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,15 @@
 import numpy as np
 
 
+def createBoxes(images, coordinates):
+    seq_length = images.shape[2]
+    for n in range(seq_length):
+        pixel_start_x = int(round(x1[n]))
+        pixel_start_y = int(round(y1[n]))
+        pixel_end_x = int(round(x2[n]))
+        pixel_end_y = int(round(y2[n]))
+        for ri in range(pixel_end_y-pixel_start_y+1):
+            for ci in range(pixel_end_x-pixel_start_x+1):
+                images[pixel_start_y+ri, pixel_start_x+ci, n] = 1
+    return images
+
