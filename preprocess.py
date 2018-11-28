52ed2797b443112df341492b5b2b0051a040c620
ZhanzhanZhao <zhanzhao@umich.edu>
2018-11-27 22:29:18 -0500

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,29 @@
-import numpy as np
+
+import os
+
+
+
+
+def GetCoordinate():
+    directory = './label_2'
+    coordinate = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            cars = []
+            with open('./label_2/000002.txt') as o:
+                for i, t in enumerate(o.readlines(), 1):
+                    l_s = t.split()
+                    dic = {}
+                    if l_s[0] == 'Car':
+                        dic['x1'] = l_s[4]
+                        dic['y1'] = l_s[5]
+                        dic['x2'] = l_s[6]
+                        dic['y2'] = l_s[7]
+                        cars.append(dic)
+        coordinate.append(cars)
+
+
+
 
 
 def createBoxes(images, coordinates):
