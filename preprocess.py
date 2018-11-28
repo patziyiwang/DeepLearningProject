3797b63baaea9b48de3d56b78b2ca006d58ea4ed
Pat Wang <patwang@lawn-128-61-124-201.lawn.gatech.edu>
2018-11-28 13:14:30 -0500

    pre2

--- preprocess.py
+++ preprocess.py
@@ -24,24 +24,22 @@ def GetCoordinate():
             return coordinate
         except IOError as exc:
             if exc.errno != errno.EISDIR:
+                print("error occurred here")
                 raise
 
-
-
-        
-
-
-
 def createBoxes(images, coordinates):
-    seq_length = images.shape[2]
+    seq_length = len(coordinates)
     for n in range(seq_length):
-        pixel_start_x = int(round(x1[n]))
-        pixel_start_y = int(round(y1[n]))
-        pixel_end_x = int(round(x2[n]))
-        pixel_end_y = int(round(y2[n]))
-        for ri in range(pixel_end_y-pixel_start_y+1):
-            for ci in range(pixel_end_x-pixel_start_x+1):
-                images[pixel_start_y+ri, pixel_start_x+ci, n] = 1
+        frame = seq_length[n]
+        for i in range(len(frame)):
+            car = frame[i]
+            pixel_start_x = int(round(car['x1']))
+            pixel_start_y = int(round(car['y1']))
+            pixel_end_x = int(round(car['x2']))
+            pixel_end_y = int(round(car['y2']))
+            for ri in range(pixel_end_y-pixel_start_y+1):
+                for ci in range(pixel_end_x-pixel_start_x+1):
+                    images[pixel_start_y+ri, pixel_start_x+ci, n] = 1
     return images
 
 
