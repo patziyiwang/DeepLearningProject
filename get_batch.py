aa8beb1cdab01794ff085cfabaea64782910cc27
zhanzhanzhao <zhanzhao@umich.edu>
2018-11-28 21:45:58 -0500

    Revert "Merge branch 'master' of /Users/zhanzhanzhao/Dropbox/deep learning project/code/DeepLearningProject with conflicts."
    This reverts commit 322863d

--- get_batch.py
+++ get_batch.py
@@ -1,35 +1,30 @@
+
 from torch.utils.data import Dataset, DataLoader
 import pickle
-import errno
-import glob
-import numpy as np
-
+import preprocess
+from preprocess import *
 
 
 class AutorallyDataset(Dataset):
   'Characterizes a dataset for PyTorch'
-  def __init__(self, ):
-      self.dataset = []
-      self.make_dataset()
+  def __init__(self, list_IDs, labels):
+        self.dataset = []
+        self.make_dataset()
 
 
 
   def make_dataset(self):
       self.dataset = []
-      length = 10
-      ##we want to get images here img
-      num = images.shape[2]
-      for i in range(num-length-1):
-          self. dataset.append({"x": images[:,:,i:i+length-1], "y": images[:,:,i+1, i+length]})
+      ele_dict = {"x": }
 
   def __len__(self):
-      'Denotes the total number of samples'
-      return len(self.dataset)
+        'Denotes the total number of samples'
+        return len(self.dataset)
 
   def __getitem__(self, index):
-       'Generates one sample of data'
-       # Select sample
-       return self.dataset[index]
+        'Generates one sample of data'
+        # Select sample
+        return self.dataset[index]
 
   def save(self):
       pickle.dump(self, open(self.name + ".pkl", "wb"))
@@ -37,6 +32,6 @@ class AutorallyDataset(Dataset):
 
 
 
-def main():
-    dataset = AutorallyDataset()
-    dataset.save()
+
+dataset = AutorallyDataset(bags, name = "BetaDataset")
+dataset.save()
