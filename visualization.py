50080f22b109c5e535350686678b7a6e236efb83
patwang <patziyiwang@gmail.com>
2018-12-04 17:47:20 -0500

    added teaser figure

--- visualization.py
+++ visualization.py
@@ -7,6 +7,7 @@ def visualize(image, name):
     ax = fig.add_subplot(1,1,1)
     imgplot = ax.imshow(image, cmap=mpl.cm.Greys)
     imgplot.set_interpolation('nearest')
+    plt.axis("off")
     plt.savefig(name + ".png")
     plt.close()
 
